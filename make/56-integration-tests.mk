4d0d2135b2959053b1a8960c97f8462def959bae
Zane Rockenbaugh <zane@liquid-labs.com>
2025-11-28 14:34:28 -0600

    remove allowing no tests and support the 'TEST' specifier

--- make/56-integration-tests.mk
+++ make/56-integration-tests.mk
@@ -30,7 +30,8 @@ $(FUNCTIONAL_TEST_PASS_MARKER) $(FUNCTIONAL_TEST_REPORT): dist/ai-ready-exec.js 
 	    --testMatch='**/tests/functional/**/*.test.js' \
 	    --runInBand \
 		--no-coverage \
-		--passWithNoTests 2>&1 ) \
+		$(TEST) \
+		2>&1 ) \
 	  | tee -a $(FUNCTIONAL_TEST_REPORT); \
 	  touch $(FUNCTIONAL_TEST_PASS_MARKER) )
 	@echo "✔ Functional tests passed"
@@ -66,7 +67,8 @@ $(INTEGRATION_TEST_PASS_MARKER) $(INTEGRATION_TEST_REPORT): dist/ai-ready-exec.j
 	    --runInBand \
 	    --no-coverage \
 	    --testTimeout=30000 \
-	    --passWithNoTests 2>&1 ) \
+		$(TEST) \
+		2>&1 ) \
 	  | tee -a $(INTEGRATION_TEST_REPORT); \
 	  touch $(INTEGRATION_TEST_PASS_MARKER) )
 	@echo "✔ Integration tests passed"
